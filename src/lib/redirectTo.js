c440809b8e3f57a114c4f7332f3bedf58c26832e
Gabriel <gabriel@gplopes.com>
2018-11-11 21:09:42 +0100

    deploy process

--- src/lib/redirectTo.js
+++ src/lib/redirectTo.js
@@ -1,12 +1,14 @@
 import Router from "next/router";
 
+const isClient = typeof document !== 'undefined';
+
 export default function redirectTo(destination, { res, status } = {}) {
-  if (res) {
+  if (res && res.writeHead) {
     res.writeHead(status || 302, { Location: destination });
     res.end();
   } else {
     if (destination[0] === "/" && destination[1] !== "/") {
-      Router.push(destination);
+      isClient && Router.push(destination);
     } else {
       window.location = destination;
     }
