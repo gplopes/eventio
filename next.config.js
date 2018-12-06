f003500f45073daf524b8375cac55a5f1289913b
Gabriel <gabriel@gplopes.com>
2018-12-06 19:58:41 +0100

    new config/plugins

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withSass = require("@zeit/next-sass");
+const withTypescript = require('@zeit/next-typescript')
 
+const typescript = withTypescript();
 const sass = withSass();
+
 module.exports = {
+  ...typescript,
   ...sass,
   onDemandEntries: {
     // on dev, since our pages are so expensive, lets keep them for 24 hours
